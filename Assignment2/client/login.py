ef89fefb51257dc6df032f5f253c87d10ad68766
Nolan Burfield <nburf@nevada.unr.edu>
2015-03-23 20:11:25 -0700

    login fixed

--- Assignment2/client/login.py
+++ Assignment2/client/login.py
@@ -19,50 +19,35 @@ class login:
       self.connection.timeout(10.0)
       error = self.connection.recieve()
       self.connection.close()
-      self.connection.TCPConnect()
-      self.connection.send(Packet().build("SEARCH " + " " , ""))
-      Log().activity("Sent the packet: SEARCH " + " ")
-
-      data = self.connection.recieve()
-      header, meta, body = Packet().divide(data)
-      while len(body) < int(meta[0]):
-        data += self.connection.recieve()
-      self.connection.close()
-   
-      header, meta, body = Packet().divide(data)
-      f = re.compile('<find>([^)]*)</find>').search(body)
-      for u in f.groups():
-        value = re.compile('<user_id>([^)]*)</user_id>').search(u)
-        try:
-          f = Friend.select().where((Friend.friend_id == value.groups()[0]))[0]
-          value = re.compile('<ip>([^)]*)</ip>').search(u)
-          f.ip = value.groups()[0]
-          f.save()
-        except:
-          pass
-      return error, " "
     except:
       self.connection.close()
-      self.connection.TCPConnect()
-      self.connection.send(Packet().build("SEARCH " + " " , ""))
-      Log().activity("Sent the packet: SEARCH " + " ")
 
-      data = self.connection.recieve()
-      header, meta, body = Packet().divide(data)
-      while len(body) < int(meta[0]):
-        data += self.connection.recieve()
-      self.connection.close()
-   
-      header, meta, body = Packet().divide(data)
-      f = re.compile('<find>([^)]*)</find>').search(body)
-      for u in f.groups():
-        value = re.compile('<user_id>([^)]*)</user_id>').search(u)
-        try:
-          f = Friend.select().where((Friend.friend_id == value.groups()[0]))[0]
-          value = re.compile('<ip>([^)]*)</ip>').search(u)
-          print value.groups()[0]
-          f.ip = value.groups()[0]
-          f.save()
-        except:
-          pass
+    self.connection.TCPConnect()
+    self.connection.send(Packet().build("SEARCH " + " " , ""))
+    Log().activity("Sent the packet: SEARCH " + " ")
+
+    data = self.connection.recieve()
+    header, meta, body = Packet().divide(data)
+    while len(body) < int(meta[0]):
+      data += self.connection.recieve()
+    self.connection.close()
+ 
+    header, meta, body = Packet().divide(data)
+    f = body.split("</find>")
+    print "FIND\n", f
+    for u in f:
+      value = re.compile('.*<user_id>([^)]*)</user_id><.*').findall(u)
+      print "VALUE\n", value
+      try:
+        f = Friend.select().where((Friend.friend_id == value[0]))[0]
+        value = re.compile('<ip>([^)]*)</ip>').search(u)
+        print value.groups()[0]
+        f.ip = value.groups()[0]
+        f.save()
+      except:
+        pass
+
+    if error:
+      return error, " "
+    else:
       return None, None    
